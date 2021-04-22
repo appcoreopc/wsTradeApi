05d9c6aaed10c373e18a8a1e76ebba4955655b08
PELCORP\X1100543 <jeremy.woowengkeong@countdown.co.nz>
2021-04-23 08:53:33 +1200

    setup simple example

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # wsTradeApi
+
+This is a starter project that uses µWebSockets. 
+
+To test 
+
+npm install -g wscat then
+
+wscat -c ws://localhost:9001/
+
+Alternatively, you can open up, index.html under the test folder. 
+
--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,23 @@ const app = uWS./*SSL*/App({
     key_file_name: 'misc/key.pem',
     cert_file_name: 'misc/cert.pem',
     passphrase: '1234'
-  }).get('/*', (res, req) => {
+  })
+  .ws('/*', {
+
+    /* There are many common helper features */
+    idleTimeout: 30,
+    maxBackpressure: 1024,
+    maxPayloadLength: 512,
+  
+    /* For brevity we skip the other events (upgrade, open, ping, pong, close) */
+    message: (ws, message, isBinary) => {
+      /* You can do app.publish('sensors/home/temperature', '22C') kind of pub/sub as well */
+      
+      /* Here we echo the message back, using compression if available */
+      let ok = ws.send(message, isBinary, true);
+    }    
+  })  
+  .get('/*', (res, req) => {
     res.end('Hello World!');
   }).listen(targetPort, (token) => {
     if (token) {
